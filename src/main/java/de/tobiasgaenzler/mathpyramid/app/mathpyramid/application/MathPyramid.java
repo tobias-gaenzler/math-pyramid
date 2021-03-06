cd47d7263a5cab243deb703be9514a73f2caa8b0
Tobias <1056318+tobias-gaenzler@users.noreply.github.com>
2021-03-06 20:13:59 +0100

    Added javadoc. Made fields final. Throw exception for invalid constructor parameters. Converted iterator into for loop. Simplified getIndex.

--- src/main/java/de/tobiasgaenzler/mathpyramid/app/mathpyramid/application/MathPyramid.java
+++ src/main/java/de/tobiasgaenzler/mathpyramid/app/mathpyramid/application/MathPyramid.java
@@ -7,155 +7,176 @@ import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * Creates a 'MathPyramid' consisting of blocks which contain integer numbers where
+ * the sum of two adjacent blocks equals the value in the block above these two blocks.
+ * 'size': number of rows must be between three and 5 (size two is too simple, above size 5 is too expensive
+ * 'maxValue': maximal integer value in one of the pyramid blocks
+ */
 public class MathPyramid {
 
-	private final Integer size; // number or rows, higher number increases difficulty
-	private final Integer maxValue; // maximum number in any block, higher numbers increase difficulty
-	private List<Integer> startValues = new ArrayList<>(); // numbers which are visible from the start
-	private List<Integer> solution =  new ArrayList<>();
+    private final Integer size; // number or rows, higher number increases difficulty
+    private final Integer maxValue; // maximum number in any block, higher numbers increase difficulty
+    private final List<Integer> startValues; // numbers which are visible from the start
+    private final List<Integer> solution;
 
-	public MathPyramid(Integer size, Integer maxValue) {
-		this.size = size;
-		this.maxValue = maxValue;
-		create();
-	}
+    public MathPyramid(Integer size, Integer maxValue) {
+        if (size < 3 || size > 5) {
+            String message = MessageFormat.format("Illegal size {0} for pyramid. " +
+                    "Size must be between 3 and 5", size);
+            throw new IllegalArgumentException(message);
+        }
+        if (maxValue < 10) {
+            String message = MessageFormat.format("Illegal maxValue {0} for pyramid. " +
+                    "MaxValue must be 10 or higher.", maxValue);
+            throw new IllegalArgumentException(message);
+        }
+        this.size = size;
+        this.maxValue = maxValue;
+        this.solution = createRandomSolution();
+        this.startValues = getRandomStartValues();
+    }
 
-	public int getSize() {
-		return size;
-	}
+    public int getSize() {
+        return size;
+    }
 
-	public int getIndex(Integer rowId, Integer colId) {
-		// starting in bottom row left, e.g. for pyramid of size 3:
-		// 0 0 -> 0
-		// 0 1 -> 1
-		// 0 2 -> 2
+    public int getIndex(Integer rowId, Integer colId) {
+        // starting in bottom row left, e.g. for pyramid of size 3:
+        // 0 0 -> 0
+        // 0 1 -> 1
+        // 0 2 -> 2
 
-		// 1 0 -> 3
-		// 1 1 -> 4
+        // 1 0 -> 3
+        // 1 1 -> 4
 
-		// 2 0 -> 5
-		if(rowId >= size) {
-			String message = MessageFormat.format("rowId {0} must be smaller than the size of the pyramid {1}", rowId, size);
-			throw new IllegalArgumentException(message);
-		}
-		if(colId >= size - rowId) {
-			String message = MessageFormat.format("colId {0} must be smaller than the size of the pyramid minus rowId {1}, size {2}", colId, rowId, size);
-			throw new IllegalArgumentException(message);
-		}
+        // 2 0 -> 5
+        if (rowId >= size) {
+            String message = MessageFormat.format("rowId {0} must be smaller than the size of the pyramid {1}", rowId, size);
+            throw new IllegalArgumentException(message);
+        }
+        if (colId >= size - rowId) {
+            String message = MessageFormat.format("colId {0} must be smaller than the size of the pyramid minus rowId {1}, size {2}", colId, rowId, size);
+            throw new IllegalArgumentException(message);
+        }
 
-		AtomicInteger index = new AtomicInteger(0);
-		IntStream.iterate(0, i -> i + 1)//
-				.limit(rowId)// increase index by (size - i) for each row
-				.forEach(i -> index.set(index.get() + size - i));
-		return index.get() + colId;
-	}
+        int index = 0;
+        // increase index by (size - i) for each row
+        for (int i = 0; i < rowId; i = i + 1) {
+            index = index + size - i;
+        }
+        return index + colId;
+    }
 
-	public String getSolutionAt(Integer rowId, Integer colId) {
-		return solution.get(getIndex(rowId, colId)).toString();
-	}
+    public String getSolutionAt(Integer rowId, Integer colId) {
+        return solution.get(getIndex(rowId, colId)).toString();
+    }
 
-	public boolean isPrefilledBlock(int row, int column) {
-		if (row < size && column < size - row) {
-			return startValues.get(getIndex(row, column)) != null;
-		}
-		return false;
-	}
+    public boolean isPrefilledBlock(int row, int column) {
+        if (row < size && column < size - row) {
+            return startValues.get(getIndex(row, column)) != null;
+        }
+        return false;
+    }
 
-	private void create() {
-		this.solution = createRandomSolution();
-		this.startValues = createStartValuesFromSolution();
-	}
+    private List<Integer> createRandomSolution() {
+        // prevent too easy solutions: increase min value?
+        int maxValueInLowestRow = Math.max(2, (int) (maxValue / Math.pow(2, size - 1)));
+        // start values in bottom row of pyramid
+        List<Integer> randomSolution = new Random()//
+                .ints(size, 1, maxValueInLowestRow)//
+                .boxed()//
+                .collect(Collectors.toList());
 
-	/* Remove 'size' values from solution */
-	private List<Integer> createStartValuesFromSolution() {
-		List<Integer> startValues = removeValuesRandomly();
-		while (!isUniquelySolvable(startValues, size)) {
-			startValues = removeValuesRandomly();
-		}
-		return startValues;
-	}
+        // solve pyramid from bottom to top
+        int offset = 0;
+        for (int i = 1; i < size; i++) {
+            for (int j = 0; j < size - i; j++) {
+                randomSolution.add(randomSolution.get(offset + j) + randomSolution.get(offset + j + 1));
+            }
+            offset += (size - (i - 1));
+        }
+        return randomSolution;
+    }
 
-	private boolean isUniquelySolvable(List<Integer> startValues, int size) {
-		int columns = getNumberOfBlocks(size);
-		int rows = columns - size;
-		SimpleMatrix A = new SimpleMatrix(rows, rows);
-		SimpleMatrix F = createMatrix();
-		int column = 0;
-		for (int i = 0; i < columns; i++) {
-			if (startValues.get(i) == null) {
-				for (int j = 0; j < rows; j++) {
-					A.set(j, column, F.get(j, i));
-				}
-				column++;
-			}
-		}
-		SimpleMatrix b = new SimpleMatrix(rows, 1);
+    /* Remove 'size' values from solution */
+    private List<Integer> getRandomStartValues() {
+        List<Integer> startValues = removeValuesRandomly();
+        while (!isUniquelySolvable(startValues, size)) {
+            startValues = removeValuesRandomly();
+        }
+        return startValues;
+    }
 
-		try {
-			A.solve(b);
-			return true;
-		} catch (SingularMatrixException e) {
-			return false;
-		}
-	}
+    private List<Integer> removeValuesRandomly() {
 
-	private SimpleMatrix createMatrix() {
+        int numberOfBlocks = getNumberOfBlocks();
+        ArrayList<Integer> startValues = new ArrayList<>(solution);
+        new Random()//
+                .ints(numberOfBlocks - size, 0, numberOfBlocks)//
+                .forEach(n -> startValues.set(n, null));
+        return startValues;
+    }
 
-		int columns = getNumberOfBlocks(size);
-		int rows = columns - size;
-		SimpleMatrix A = new SimpleMatrix(rows, columns);
+    private boolean isUniquelySolvable(List<Integer> startValues, int size) {
+        int columns = getNumberOfBlocks();
+        int rows = columns - size;
+        SimpleMatrix A = new SimpleMatrix(rows, rows);
+        SimpleMatrix F = createMatrix();
+        int column = 0;
+        for (int i = 0; i < columns; i++) {
+            if (startValues.get(i) == null) {
+                for (int j = 0; j < rows; j++) {
+                    A.set(j, column, F.get(j, i));
+                }
+                column++;
+            }
+        }
+        SimpleMatrix b = new SimpleMatrix(rows, 1);
 
-		// -1 on size-th upper diagonal
-		IntStream.iterate(0, row -> row + 1)//
-				.limit(rows)//
-				.forEach(row -> A.set(row, row + size, -1.0d));
-		// two ones on diagonal blocks
-		int row = 0;
-		for (int i = 0; i < size - 1; i++) {
-			for (int j = 0; j < size - 1 - i; j++) {
-				A.set(row + j, row + j + i, 1.0d);
-				A.set(row + j, row + j + i + 1, 1.0d);
-			}
-			row += size - i - 1;
-		}
+        try {
+            A.solve(b);
+            return true;
+        } catch (SingularMatrixException e) {
+            return false;
+        }
+    }
 
-		return A;
-	}
+    private SimpleMatrix createMatrix() {
 
-	private List<Integer> removeValuesRandomly() {
-		int numberOfBlocks = getNumberOfBlocks(size);
-		ArrayList<Integer> startValues = new ArrayList<>(solution);
-		new Random()//
-				.ints(numberOfBlocks - size, 0, numberOfBlocks)//
-				.forEach(n -> startValues.set(n, null));
-		return startValues;
-	}
+        int columns = getNumberOfBlocks();
+        int rows = columns - size;
+        SimpleMatrix A = new SimpleMatrix(rows, columns);
 
-	private List<Integer> createRandomSolution() {
-		// prevent too easy solutions: increase min value?
-		int maxValueInLowestRow = Math.max(2, (int) (maxValue / Math.pow(2, size - 1)));
-		// start values in bottom row of pyramid
-		List<Integer> randomSolution = new Random()//
-				.ints(size, 1, maxValueInLowestRow)//
-				.boxed()//
-				.collect(Collectors.toList());
+        // -1 on size-th upper diagonal
+        IntStream.iterate(0, row -> row + 1)//
+                .limit(rows)//
+                .forEach(row -> A.set(row, row + size, -1.0d));
+        // two ones on diagonal blocks
+        int row = 0;
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - 1 - i; j++) {
+                A.set(row + j, row + j + i, 1.0d);
+                A.set(row + j, row + j + i + 1, 1.0d);
+            }
+            row += size - i - 1;
+        }
 
-		// solve pyramid from bottom to top
-		int offset = 0;
-		for (int i = 1; i < size; i++) {
-			for (int j = 0; j < size - i; j++) {
-				randomSolution.add(randomSolution.get(offset + j) + randomSolution.get(offset + j + 1));
-			}
-			offset += (size - (i - 1));
-		}
-		return randomSolution;
-	}
+        return A;
+    }
 
-	private int getNumberOfBlocks(int size) {
-		return (size * size + size) / 2;
-	}
+    int getNumberOfBlocks() {
+        return (size * size + size) / 2;
+    }
+
+    List<Integer> getStartValues() {
+        return startValues;
+    }
+
+    List<Integer> getSolution() {
+        return solution;
+    }
 }
---
+++ src/test/java/de/tobiasgaenzler/mathpyramid/app/mathpyramid/application/MathPyramidTest.java
@@ -0,0 +1,80 @@
+package de.tobiasgaenzler.mathpyramid.app.mathpyramid.application;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class MathPyramidTest {
+
+    @ParameterizedTest
+    @CsvSource({"3,6", "4,10", "5,15"})
+    public void testMathPyramidCreatesPyramidWithCorrectSizing(int size, int numberOfBlocks) {
+        MathPyramid mathPyramid = new MathPyramid(size, 10000);
+        assertThat(mathPyramid.getSize()).isEqualTo(size);
+        assertThat(mathPyramid.getNumberOfBlocks()).isEqualTo(numberOfBlocks);
+        assertThat(mathPyramid.getStartValues().size()).isEqualTo(numberOfBlocks);
+        assertThat(mathPyramid.getSolution().size()).isEqualTo(numberOfBlocks);
+        // three start values
+        assertThat(mathPyramid.getStartValues().stream().filter(Objects::nonNull).count())
+                .isEqualTo(size);
+        // six solution values
+        assertThat(mathPyramid.getSolution().size()).isEqualTo(numberOfBlocks);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({"3,10", "4,100", "5,10000", "5," + Integer.MAX_VALUE})
+    public void testMathPyramidCreatesPyramidWithValuesLowerOrEqualThanMaxValue(int size, int maxValue) {
+        MathPyramid mathPyramid = new MathPyramid(size, maxValue);
+        assertThat(mathPyramid.getSolution().stream()
+                .filter(value -> value > maxValue).findFirst().isEmpty())
+                .isEqualTo(true);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {Integer.MIN_VALUE, -1, 0, 1, 2, 6, Integer.MAX_VALUE})
+    public void testIllegalSizesLessThanThreeOrGreaterThan5(int size) {
+        assertThatThrownBy(() -> new MathPyramid(size, 10000))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("between 3 and 5");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {Integer.MIN_VALUE, -1, 0, 1, 2, 9})
+    public void testIllegalMaxValuesSmallerThan10(int maxValue) {
+        assertThatThrownBy(() -> new MathPyramid(3, maxValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("10 or higher");
+    }
+
+    @Test
+    public void testMathPyramidOfSizeThreeIsValid() {
+        MathPyramid mathPyramid = new MathPyramid(3, 10000);
+        assertThat(valueAt(mathPyramid, 0, 0) +
+                valueAt(mathPyramid, 0, 1))
+                .isEqualTo(valueAt(mathPyramid, 1, 0));
+        assertThat(valueAt(mathPyramid, 0, 1) +
+                valueAt(mathPyramid, 0, 2))
+                .isEqualTo(valueAt(mathPyramid, 1, 1));
+        assertThat(valueAt(mathPyramid, 1, 0) +
+                valueAt(mathPyramid, 1, 1))
+                .isEqualTo(valueAt(mathPyramid, 2, 0));
+    }
+
+    private int valueAt(MathPyramid mathPyramid, int row, int col) {
+        return Integer.parseInt(mathPyramid.getSolutionAt(row, col));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,0,0", "0,1,1", "0,2,2", "1,0,3", "1,1,4", "2,0,5"})
+    public void testGetIndexReturnsCorrectValues(int row, int col, int index) {
+        MathPyramid mathPyramid = new MathPyramid(3, 10000);
+        assertThat(mathPyramid.getIndex(row, col)).isEqualTo(index);
+    }
+}

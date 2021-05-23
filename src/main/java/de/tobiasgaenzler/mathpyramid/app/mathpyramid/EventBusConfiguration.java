66c7c06a0acbd603f378f114847a191d784f42fb
tobias-gaenzler <tobias-gaenzler@users.noreply.github.com>
2021-05-23 17:10:45 +0200

    Use specific name for configuration.

--- src/main/java/de/tobiasgaenzler/mathpyramid/app/mathpyramid/EventBusConfiguration.java
+++ src/main/java/de/tobiasgaenzler/mathpyramid/app/mathpyramid/EventBusConfiguration.java
@@ -3,9 +3,10 @@ package de.tobiasgaenzler.mathpyramid.app.mathpyramid;
 import com.google.common.eventbus.EventBus;
 import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@org.springframework.context.annotation.Configuration
-public class Configuration {
+@Configuration
+public class EventBusConfiguration {
     @UIScope
     @Bean(name = "uiEventBus")
     public EventBus eventBus() {

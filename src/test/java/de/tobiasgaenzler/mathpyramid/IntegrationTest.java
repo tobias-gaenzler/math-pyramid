2433e00ab72814c3fe8501b37d1cfdfc16720676
Tobias Gänzler <tobias.gaenzler@fielmann.com>
2022-05-27 08:50:01 +0200

    Update spring boot version.

--- src/test/java/de/tobiasgaenzler/mathpyramid/IntegrationTest.java
+++ src/test/java/de/tobiasgaenzler/mathpyramid/IntegrationTest.java
@@ -4,18 +4,15 @@ import io.github.bonigarcia.seljup.Arguments;
 import io.github.bonigarcia.seljup.SeleniumJupiter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
+import java.time.Duration;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -44,18 +41,18 @@ class IntegrationTest {
         List<WebElement> textFields = driver.findElements(By.cssSelector("vaadin-integer-field"));
         // find shadow dom input fields (from top to bottom, left to right)
         List<WebElement> inputFields = textFields.stream().map(textField -> {
-            WebElement shadowTextField = getShadowRootElement(textField, driver);
+            SearchContext shadowTextField = getShadowRootElement(textField, driver);
             return shadowTextField.findElement(By.cssSelector("input"));
-        }).collect(Collectors.toList());
+        }).toList();
 
-        fillFieldIfEmpty(inputFields.get(0),"4");
-        fillFieldIfEmpty(inputFields.get(1),"2");
-        fillFieldIfEmpty(inputFields.get(2),"2");
-        fillFieldIfEmpty(inputFields.get(3),"1");
-        fillFieldIfEmpty(inputFields.get(4),"1");
-        fillFieldIfEmpty(inputFields.get(5),"1");
+        fillFieldIfEmpty(inputFields.get(0), "4");
+        fillFieldIfEmpty(inputFields.get(1), "2");
+        fillFieldIfEmpty(inputFields.get(2), "2");
+        fillFieldIfEmpty(inputFields.get(3), "1");
+        fillFieldIfEmpty(inputFields.get(4), "1");
+        fillFieldIfEmpty(inputFields.get(5), "1");
         // check if notification is present
-        WebDriverWait wait = new WebDriverWait(driver, 2);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("vaadin-notification-card")));
     }
 
@@ -68,13 +65,13 @@ class IntegrationTest {
 
     private void waitForApplicationAvailable(ChromeDriver driver) {
         // wait for application to start: search for element with class "app-layout"
-        WebDriverWait wait = new WebDriverWait(driver, 120);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(120));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("app-layout")));
     }
 
 
-    public WebElement getShadowRootElement(WebElement element, ChromeDriver driver) {
-        return (WebElement) ((JavascriptExecutor) driver)
+    public SearchContext getShadowRootElement(WebElement element, ChromeDriver driver) {
+        return (SearchContext) ((JavascriptExecutor) driver)
                 .executeScript("return arguments[0].shadowRoot", element);
     }
 }

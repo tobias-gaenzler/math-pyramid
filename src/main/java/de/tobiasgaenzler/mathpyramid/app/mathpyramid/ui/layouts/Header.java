df3529caf2f1946560d47e8626dd9c4ef150d15e
tobias-gaenzler <tobias-gaenzler@users.noreply.github.com>
2022-06-13 22:50:08 +0200

    Add help page.

--- src/main/java/de/tobiasgaenzler/mathpyramid/app/mathpyramid/ui/layouts/Header.java
+++ src/main/java/de/tobiasgaenzler/mathpyramid/app/mathpyramid/ui/layouts/Header.java
@@ -1,13 +1,19 @@
 package de.tobiasgaenzler.mathpyramid.app.mathpyramid.ui.layouts;
 
 import com.google.common.eventbus.EventBus;
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import de.tobiasgaenzler.mathpyramid.app.mathpyramid.ui.events.NewGameEvent;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER;
 
@@ -15,23 +21,61 @@ import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CE
 @UIScope
 public class Header extends HorizontalLayout {
 
+    private final EventBus uiEventBus;
+
     @Autowired
     public Header(EventBus uiEventBus) {
-        H1 logo = new H1("Math Pyramid");
-        logo.addClassName("logo");
-        add(logo);
+        setWidth("100%");
+        setDefaultVerticalComponentAlignment(CENTER);
+        this.uiEventBus = uiEventBus;
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        removeAll();
+        addLogo();
+        addButtons(uiEventBus);
+        addClassName("header");
+    }
+
+    private void addButtons(EventBus uiEventBus) {
+        AtomicReference<String> segment = new AtomicReference<>("");
+        getUI().ifPresent(ui ->
+                segment.set(ui.getInternals().getActiveViewLocation().getFirstSegment())
+        );
+        // no buttons on help view
+        if (segment.get().equals("help")) {
+            return;
+        }
+        HorizontalLayout buttonsLayout = new HorizontalLayout();
+        buttonsLayout.setAlignItems(Alignment.END);
 
         Button startMultiplayerGameButton = new Button("Start Multiplayer Game");
         startMultiplayerGameButton.addClickListener(event -> uiEventBus.post(new NewGameEvent(true)));
-        add(startMultiplayerGameButton);
+        buttonsLayout.add(startMultiplayerGameButton);
 
         Button startNewGameButton = new Button("Start Game");
         startNewGameButton.addClickListener(event -> uiEventBus.post(new NewGameEvent(false)));
-        add(startNewGameButton);
+        buttonsLayout.add(startNewGameButton);
 
-        addClassName("header");
-        setWidth("100%");
+        Button helpButton = new Button(new Icon(VaadinIcon.QUESTION_CIRCLE));
+        helpButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        helpButton.addClickListener(event -> getUI().ifPresent(ui -> ui.navigate("help")));
+        buttonsLayout.add(helpButton);
+
+        add(buttonsLayout);
+    }
+
+    private void addLogo() {
+        VerticalLayout logoLayout = new VerticalLayout();
+        logoLayout.setAlignItems(Alignment.START);
+        logoLayout.addClassName("logo-layout");
+        Button logo = new Button("Math Pyramid");
+        logo.addClassName("logo");
+        logo.addThemeVariants(ButtonVariant.LUMO_TERTIARY, ButtonVariant.LUMO_CONTRAST);
+        logo.addClickListener(event -> getUI().ifPresent(ui -> ui.navigate("/")));
+        logoLayout.add(logo);
+        add(logoLayout);
         expand(logo);
-        setDefaultVerticalComponentAlignment(CENTER);
     }
 }
---
+++ src/main/java/de/tobiasgaenzler/mathpyramid/app/mathpyramid/ui/views/HelpView.java
@@ -0,0 +1,72 @@
+package de.tobiasgaenzler.mathpyramid.app.mathpyramid.ui.views;
+
+import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
+import de.tobiasgaenzler.mathpyramid.app.mathpyramid.ui.layouts.MainLayout;
+
+import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER;
+
+@Route(value = "help", layout = MainLayout.class)
+@PageTitle("Help")
+public class HelpView extends VerticalLayout {
+
+    public HelpView() {
+        setAlignItems(CENTER);
+        addClassName("help");
+
+        addTitle();
+
+        addRules();
+
+        addImages();
+
+        addLinks();
+    }
+
+    private void addLinks() {
+        VerticalLayout linkList = new VerticalLayout();
+        linkList.add(new H5("Links"));
+        linkList.add(new Anchor("https://www.youtube.com/watch?v=oLcpiQYtwYg", new ListItem("Tutorial (english)")));
+        linkList.add(new Anchor("https://www.youtube.com/watch?v=PMCsU79CI30", new ListItem("Tutorial (german)")));
+        linkList.setAlignItems(CENTER);
+        add(linkList);
+    }
+
+    private void addImages() {
+        HorizontalLayout imageLayout = new HorizontalLayout();
+        imageLayout.addClassName("image-layout");
+        StreamResource helpStartImageResource = new StreamResource("help_start.jpg",
+                () -> getClass().getResourceAsStream("/images/help_start.jpg"));
+        Image helpStartImage = new Image(helpStartImageResource, "Help Start");
+        helpStartImage.setMaxWidth(25, Unit.EM);
+        imageLayout.add(helpStartImage);
+
+        Icon icon = new Icon("arrow-right");
+        imageLayout.add(icon);
+
+        StreamResource helpFinishImageResource = new StreamResource("help_finished.jpg",
+                () -> getClass().getResourceAsStream("/images/help_finished.jpg"));
+        Image helpFinishedImage = new Image(helpFinishImageResource, "Help Finished");
+        helpFinishedImage.setMaxWidth(25, Unit.EM);
+        imageLayout.add(helpFinishedImage);
+        add(imageLayout);
+    }
+
+    private void addRules() {
+        add(new UnorderedList(
+                new ListItem("Rule 1: the sum of the numbers in two adjacent blocks must equal the number in the block above the two adjacent blocks "),
+                new ListItem("Rule 2: the pyramid is solved when all blocks are filled and rule 1 holds for all adjacent blocks ")
+        ));
+    }
+
+    private void addTitle() {
+        H3 title = new H3("How to play Math-Pyramid?");
+        add(title);
+    }
+}

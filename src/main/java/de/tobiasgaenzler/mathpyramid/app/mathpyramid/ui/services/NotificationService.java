21f10040d74f840514104b61eec7b9eedc90a7e9
tobias-gaenzler <tobias-gaenzler@users.noreply.github.com>
2022-06-19 22:36:40 +0200

    Center fields in notification.

--- src/main/java/de/tobiasgaenzler/mathpyramid/app/mathpyramid/ui/services/NotificationService.java
+++ src/main/java/de/tobiasgaenzler/mathpyramid/app/mathpyramid/ui/services/NotificationService.java
@@ -4,21 +4,27 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import org.springframework.stereotype.Service;
+
+import static com.vaadin.flow.component.notification.Notification.Position.MIDDLE;
+import static com.vaadin.flow.component.notification.NotificationVariant.LUMO_SUCCESS;
+import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER;
 
 @Service
 public class NotificationService {
     public Notification createNotification(String message) {
         VerticalLayout layout = new VerticalLayout();
+        layout.setAlignItems(CENTER);
         Span content = new Span(message);
         layout.add(content);
         Button closeButton = new Button("Close");
         layout.add(closeButton);
         Notification notification = new Notification(layout);
         notification.setDuration(5_000);
-        notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
-        notification.setPosition(Notification.Position.MIDDLE);
+        notification.addThemeVariants(LUMO_SUCCESS);
+        notification.setPosition(MIDDLE);
         closeButton.addClickListener(closeButtonClickedEvent -> notification.close());
         return notification;
     }

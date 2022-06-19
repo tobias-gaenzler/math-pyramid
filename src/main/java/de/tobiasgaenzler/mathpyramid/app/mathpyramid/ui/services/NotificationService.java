6527ae4b099bfc6698fb3959d03982b1de49776d
tobias-gaenzler <tobias-gaenzler@users.noreply.github.com>
2022-06-19 22:31:52 +0200

    Hide notifications after delay.

--- src/main/java/de/tobiasgaenzler/mathpyramid/app/mathpyramid/ui/services/NotificationService.java
+++ src/main/java/de/tobiasgaenzler/mathpyramid/app/mathpyramid/ui/services/NotificationService.java
@@ -3,6 +3,7 @@ package de.tobiasgaenzler.mathpyramid.app.mathpyramid.ui.services;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,8 @@ public class NotificationService {
         Button closeButton = new Button("Close");
         layout.add(closeButton);
         Notification notification = new Notification(layout);
-        notification.setDuration(0);
+        notification.setDuration(5_000);
+        notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
         notification.setPosition(Notification.Position.MIDDLE);
         closeButton.addClickListener(closeButtonClickedEvent -> notification.close());
         return notification;
